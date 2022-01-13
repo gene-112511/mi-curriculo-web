bb7441995b94ff8dbbd62475162cb917a3658b31
gene-112511 <81432310+gene-112511@users.noreply.github.com>
2022-01-13 08:41:18 -0800

    Update mandoMail.php

--- public/mandoMail.php
+++ public/mandoMail.php
@@ -4,7 +4,7 @@ if ($_POST['g-recaptcha-response'] == '') {
 echo "Captcha invalido";
 } else {
 $obj = new stdClass();
-$obj->secret = "6LfRSNISAAAAACKaHw2e-JvgeG-3src_dRGpL-Ql";
+$obj->secret = "6Lf9JQ4eAAAAADsdOIhr5jiLgxbF9qlYoZSTYhmF";
 $obj->response = $_POST['g-recaptcha-response'];
 $obj->remoteip = $_SERVER['REMOTE_ADDR'];
 $url = 'https://www.google.com/recaptcha/api/siteverify';
@@ -21,18 +21,18 @@ $result = file_get_contents($url, false, $context);
 
 $validar = json_decode($result);
 
+
 /* FIN DE CAPTCHA */
 
 if ($validar->success) {
 $email = trim($_POST['email']);
-$nombre = trim($_POST['nombre']);
-$apellido = trim($_POST['apellido']);
-$telefono = trim($_POST['telefono']);
-$comentario = trim($_POST['comentario']);
+$nombre = trim($_POST['name']);
+$apellido = trim($_POST['subject']);
+$comentario = trim($_POST['message']);
 
-$consulta = "E-mail: " . $email . " Nombre: " . $nombre . " Apellido: " . $apellido . "Comentario: " . $comentario;
+$consulta = "E-mail: " . $email . " Nombre: " . $name . "Mensaje: " . $message;
 
-mail("ejemplo@MiDominio.com", "Contacto desde Formulario", $consulta);
+mail("tonygabrielramirez98@gmail.com", "Contacto desde Formulario", $consulta);
 } else {
 echo "Captcha invalido";
 }
